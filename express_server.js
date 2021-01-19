50b3b2634662fd164d994597370161e44688ae19
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-19 20:13:41 +0000

    Add post method for URLs

--- express_server.js
+++ express_server.js
@@ -13,17 +13,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
 
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
+function generateRandomString() {
+  let r = Math.random(9).toString(36).substring(6);
+  return r;
+}
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+app.post("/urls", (req, res) => { 
+  console.log(req.body); // Log the POST request body to the console
+  const shortURL = generateRandomString()
+  const longURL = req.body.longURL
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls")
 });
 
 
@@ -45,12 +46,6 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
-
-
-function generateRandomString() {
-  let r = Math.random(9).toString(36).substring(6);
-  return r;
-}
 
 
 app.listen(PORT, () => {
