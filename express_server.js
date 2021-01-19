e41f52cb805981e71218f896010b9175828e7d2e
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-19 21:18:25 +0000

    Completed Part 1 URL Shortening

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const PORT = 8080; // default port 8080
 
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -13,33 +13,37 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-
 function generateRandomString() {
   let r = Math.random(9).toString(36).substring(6);
   return r;
 }
-
-app.post("/urls", (req, res) => { 
-  console.log(req.body); // Log the POST request body to the console
-  const shortURL = generateRandomString()
-  const longURL = req.body.longURL
-  urlDatabase[shortURL] = longURL;
-  res.redirect("/urls")
-});
-
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 // present form to the user
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  const shortURL = generateRandomString()
+  const longURL = req.body.longURL
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls/")
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  //console.log(req.params);
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
