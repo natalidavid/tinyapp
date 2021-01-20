5db59b89006c2e1e2209d74495c559112a999fc7
Natalie Davidovic <natalija.david@hotmail.com>
2021-01-20 05:32:36 +0000

    Add cookie parser

--- express_server.js
+++ express_server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-
+// body parser setup
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+
+// cookie parser setup
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -54,10 +59,6 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls")
 })
 
-
-/* GET */
-
-
 app.get("/u/:shortURL", (req, res) => {
   //console.log(req.params);
   const longURL = urlDatabase[req.params.shortURL]
@@ -71,7 +72,6 @@ app.get("/urls/:shortURL", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
